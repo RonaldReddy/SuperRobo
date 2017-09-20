b505810c517f0a445a301d91f1bfcfcefd8c896a
RonaldReddy <32142817+RonaldReddy@users.noreply.github.com>
2017-09-20 12:00:47 -0700

    Initial commit

--- Program.cs
+++ Program.cs
@@ -10,15 +10,19 @@ namespace TestClinet
 {
     class Program
     {
-        public static int nochips = 2;
         static void Main(string[] args)
         {
             int[] array = new int[] { 3, 1, 4, 5, 2 };
             string  order = "desc";
             SuperRobot.Task a = new SuperRobot.Task();
+
+            //Testing total no of chips
             int count = a.nochips;
+            //Testing array sorting
             array = a.sortArray(array, order);
-            int totalelements =  a.SumOfArray(array);
+            //Testing Sum of array
+            int Sum =  a.SumOfArray(array);
+            //Testing donotreturnanything
             a.donotreturnanything();
         }
     }
