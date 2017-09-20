6065509592e1337170b9d1eb08aa5efddb45ff51
RonaldReddy <32142817+RonaldReddy@users.noreply.github.com>
2017-09-20 11:50:37 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuperRobot;
+
+
+namespace TestClinet
+{
+    class Program
+    {
+        public static int nochips = 2;
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 3, 1, 4, 5, 2 };
+            string  order = "desc";
+            SuperRobot.Task a = new SuperRobot.Task();
+            int count = a.nochips;
+            array = a.sortArray(array, order);
+            int totalelements =  a.SumOfArray(array);
+            a.donotreturnanything();
+        }
+    }
+}
